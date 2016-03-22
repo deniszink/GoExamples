8cb9d8c5cdb47e2fbf72f6c98ef4d2b01f41fa79
Denis Zinkovsiy <joker.ua1@gmail.com>
2016-03-22 18:09:46 +0200

    added rcp and mutex examples

---
+++ src/concurrency/Mutex.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+)
+
+func main() {
+	m := new(sync.Mutex)
+	for i := 0; i < 10; i++ {
+		go func(i int){
+			m.Lock()
+			fmt.Print(i," start ")
+			time.Sleep(time.Second)
+			fmt.Println(i,"end")
+			m.Unlock()
+		}(i)
+	}
+
+	var input string
+	fmt.Scanln(&input)
+}
